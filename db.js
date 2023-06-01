3d26eb8cd1dd7da9ca87ba47db45e2c82061205b
as7ro <orkhangk@code.edu.az>
2023-06-01 23:29:18 +0400

    async add

--- db.js
+++ db.js
@@ -1,16 +1,16 @@
-import mongoose from "mongoose";
+    import mongoose from "mongoose";
 
 
-const conn = ()=>{
-    mongoose.connect(process.env.DB_URI,{
-        dbName: "damv",
-        useNewUrlParser:true,
-        useUnifiedTopology:true,
-    }).then(()=>{
-        console.log("connected damv db")
-    }).catch((err)=>{
-        console.log("error verdi",err)
-    })
-}
+    const conn = async()=>{
+       await mongoose.connect(process.env.DB_URI,{
+            dbName: "damv",
+            useNewUrlParser:true,
+            useUnifiedTopology:true,
+        }).then(()=>{
+            console.log("connected damv db")
+        }).catch((err)=>{
+            console.log("error verdi",err)
+        })
+    }
 
-export default conn;
+    export default conn;
