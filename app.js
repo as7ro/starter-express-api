a40ff33688c82eab7aef99110439488824fb0527
as7ro <orkhangk@code.edu.az>
2023-06-02 01:16:54 +0400

    cloudinary

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ import session from 'express-session';
 dotenv.config();
 //connection to the db
 
-
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+  // secure:true
+})
 
 conn();
 
